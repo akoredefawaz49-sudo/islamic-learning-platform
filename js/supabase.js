946af11380cac2d5b2b1b2650b57d7a16db76719
Fawaz Grafix <akoredefawaz49@gmail.com>
2026-09-07 13:44:57 +0100

    Initialize Supabase client with URL and key

---
+++ js/supabase.js
@@ -0,0 +1,12 @@
+const SUPABASE_URL =
+    "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE";
+
+const SUPABASE_KEY =
+    "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE";
+
+
+const supabaseClient =
+    supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
